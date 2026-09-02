c132e2803a55678ae23354063b800eb797d2917c
Saitheja20 <komallasaiteja@gmail.com>
2026-09-02 17:51:04 +0530

    Add debugbar JSON files for newsletter page requests
    - Created debugbar_1788328873.732082.json for the first request to the newsletters page, capturing timing, memory usage, and route details.
    - Created debugbar_1788331003.952076.json for the second request to the newsletters page, including similar metrics and additional event data.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,3 +4,5 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+$routes->get('index3', 'Home::index3');
+$routes->get('newsletters', 'Home::newsletters');
